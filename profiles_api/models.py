a34e510e496ede8bb4b3ccbe9cb2327141bcb4b9
sohagmahin <sohagmahin@gmail.com>
2020-08-23 17:56:42 +0600

    Fixed no normalize attribute error

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -11,7 +11,7 @@ class UserProfileManager(BaseUserManager):
         if not email:
             raise ValueError('User must have an email address')
 
-        email = self.normalize(email)
+        email = self.normalize_email(email)
         user = self.model(email=email,name=name)
 
         user.set_password(password)
